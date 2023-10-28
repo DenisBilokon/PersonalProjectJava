c53bd2c76f1ef3c55c672780ef9f6d9be102e369
DenisBilokon <Superden994@gmail.com>
2023-10-28 14:47:42 +0300

    1st

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,44 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
             System.out.print("Введіть ім'я студента: ");
-        String name = scanner.nextLine();
+            String name = scanner.nextLine();
 
-        System.out.print("Введіть кількість оцінок: ");
-        int numGrades = scanner.nextInt();
-        int[] grades = new int[numGrades];
+            System.out.print("Введіть кількість оцінок: ");
+            int numGrades = scanner.nextInt();
+            int[] grades = new int[numGrades];
 
-        for (int i = 0; i < numGrades; i++) {
-            System.out.print("Введіть оцінок " + (i + 1) + ": ");
-            grades[i] = scanner.nextInt();
+            for (int i = 0; i < numGrades; i++) {
+                System.out.print("Введіть оцінку " + (i + 1) + ": ");
+                grades[i] = scanner.nextInt();
+            }
+
+            System.out.print("Виберіть тип студента (1 - Бакалавр, 2 - Магістр): ");
+            int studentType = scanner.nextInt();
+
+            Student student;
+
+            if (studentType == 1) {
+                student = new UndergraduateStudent(name, grades);
+            } else if (studentType == 2) {
+                student = new GraduateStudent(name, grades);
+            } else {
+                System.out.println("Невірний вибір типу студента.");
+                return;
+            }
+
+            student.displayStudentInfo();
+
+            System.out.print("Бажаєте ввести іншого студента? (1 - так, 2 - ні): ");
+            int anotherStudent = scanner.nextInt();
+            if (anotherStudent != 1) {
+                break;
+            }
+
+            // Очистка буфера вводу
+            scanner.nextLine();
         }
-
-        System.out.print("Виберіть тип студента (1 - Бакалавр, 2 - Магістр): ");
-        int studentType = scanner.nextInt();
-
-        Student student;
-
-        if (studentType == 1) {
-            student = new UndergraduateStudent(name, grades);
-        } else if (studentType == 2) {
-            student = new GraduateStudent(name, grades);
-        } else {
-            System.out.println("Невірний вибір типа студента.");
-            return;
-        }
-
-        student.displayStudentInfo();
 
         scanner.close();
     }
